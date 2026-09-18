0694cc5fffd3ba9b0f0eed96bf1ceae9fffa0b4b
Kevvceo <kevv246810@gmail.com>
2026-09-18 17:56:11 +0700

    Update order.js

--- api/order.js
+++ api/order.js
@@ -1,10 +1,8 @@
 const axios = require('axios');
-const crypto = require('crypto');
 
 module.exports = async (req, res) => {
     const API_KEY = process.env.VIPAYMENT_KEY;
-    const MERCHANT_ID = process.env.VIPAYMENT_MERCHANT;
-    const sign = crypto.createHash('md5').update(API_KEY + MERCHANT_ID).digest('hex');
+    const SIGN = process.env.VIPAYMENT_SIGN;
 
     const serviceCode = req.body.serviceCode;
     const userId = req.body.userId;
@@ -17,7 +15,7 @@ module.exports = async (req, res) => {
     try {
         const params = new URLSearchParams();
         params.append('key', API_KEY);
-        params.append('sign', sign);
+        params.append('sign', SIGN);
         params.append('type', 'order');
         params.append('service', serviceCode);
         params.append('data_no', userId);
@@ -30,16 +28,11 @@ module.exports = async (req, res) => {
         );
 
         const data = response.data;
-        const success = data.result || false;
-        const message = data.message || "Terjadi kesalahan";
-        const trxId = (data.data && data.data.trxid) ? data.data.trxid : "";
-
         res.json({
-            success: success,
-            message: message,
-            trxId: trxId
+            success: data.result || false,
+            message: data.message || "Terjadi kesalahan",
+            trxId: (data.data && data.data.trxid) ? data.data.trxid : ""
         });
-
     } catch (error) {
         res.json({ success: false, message: error.message });
     }
