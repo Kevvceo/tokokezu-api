4be43967c9e1d62f21e1cf2e7a6b4d18c0e3fabb
Kevvceo <kevv246810@gmail.com>
2026-09-18 20:28:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ app.use(express.json());
 const balanceRoute = require('./api/balance.js');
 const orderRoute = require('./api/order.js');
 const nicknameRoute = require('./api/nickname.js');
+const notifyRoute = require('./api/notify.js');
 
 app.all('/api/balance', (req, res) => balanceRoute(req, res));
-app.all('/api/profile', (req, res) => balanceRoute(req, res)); // Tambahan untuk cek profile/saldo
+app.all('/api/profile', (req, res) => balanceRoute(req, res));
 app.all('/api/order', (req, res) => orderRoute(req, res));
-app.all('/api/nickname', (req, res) => nicknameRoute(req, res)); // Mengaktifkan fitur nickname
+app.all('/api/nickname', (req, res) => nicknameRoute(req, res));
+app.post('/api/notify', (req, res) => notifyRoute(req, res)); // Fitur kirim notif
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
