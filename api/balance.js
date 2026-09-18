62e46fbfb39dada1ee0b186a4f421991d3b8ad19
Kevvceo <kevv246810@gmail.com>
2026-09-18 17:55:53 +0700

    Update balance.js

--- api/balance.js
+++ api/balance.js
@@ -1,15 +1,13 @@
 const axios = require('axios');
-const crypto = require('crypto');
 
 module.exports = async (req, res) => {
     const API_KEY = process.env.VIPAYMENT_KEY;
-    const MERCHANT_ID = process.env.VIPAYMENT_MERCHANT;
-    const sign = crypto.createHash('md5').update(API_KEY + MERCHANT_ID).digest('hex');
+    const SIGN = process.env.VIPAYMENT_SIGN;
 
     try {
         const params = new URLSearchParams();
         params.append('key', API_KEY);
-        params.append('sign', sign);
+        params.append('sign', SIGN);
 
         const response = await axios.post(
             'https://vip-reseller.co.id/api/profile',
